0cc469bdbf7428e8e4894ae0f620a06223491b08
Shrey K <srkkadia@gmail.com>
2025-06-27 16:45:52 +0530

    Writing File

---
+++ File Handling/writingFile.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<fstream>   //ifstream + ofstream
+using namespace std;
+
+int main() {
+    string fileName;
+
+    cout << "Enter file name: ";
+    cin >> fileName;
+    fileName.append(".txt");
+
+    // creating a file for writing
+    fstream fout;
+    // ofstream fout(fileName);
+    
+    fout.open(fileName, ios::out);
+
+    if(!fout.is_open()) {
+        cout << "Error opening " << fileName << " for writing\n";
+    } else {
+        string line;
+
+        cout << "Enter data: ";
+        getline(cin, line);
+        while(line != "~") {
+            fout << line << endl;
+            getline(cin, line);
+        }
+        cout << "Data written into " << fileName << " successfully!" << endl;
+        fout.close();   //close the file stream
+    }
+
+    return 0;
+}
